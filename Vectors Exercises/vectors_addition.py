7bdc38d48138d679c94e2dfb17cc5c393ac88263
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-07-06 17:33:44 -0500

    Create vectors_addition.py
    this is a simple exercise about addition vectors

---
+++ Vectors Exercises/vectors_addition.py
@@ -0,0 +1,12 @@
+
+# define vectors as tuples
+a = (3,2)
+b = (-1,4)
+
+# add component by component
+
+addition = (a[0] + b[0], a[1] + b[1])
+
+print("Vector a: ", a)
+print("Vector b: ", b)
+print("result: ", addition)
