f29e22dc68ba1533be344daea49b1e7cf18cbefa
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2025-07-09 23:13:15 -0500

    Update calculate_angle_between_vectors.py
    add poo in order to implement the method 'calculate_dot_product(v1,v2)' in another file

--- Vectors Exercises/calculate_angle_between_vectors.py
+++ Vectors Exercises/calculate_angle_between_vectors.py
@@ -1,10 +1,18 @@
 import math
-def calculate_angle_between_vectors(v1,v2):
-   
+
+def calculate_dot_product(v1,v2):
    x1, y1 = v1
    x2, y2 = v2
 
    dot_product = ( (v1[0]*v2[0]) + (v1[1]*v2[1]) )
+   return dot_product
+
+def calculate_angle_between_vectors(v1,v2):
+   
+   '''x1, y1 = v1
+   x2, y2 = v2
+
+   dot_product = ( (v1[0]*v2[0]) + (v1[1]*v2[1]) )'''
 
    sum_exponent_v1 = (math.pow(v1[0],2))+(math.pow(v1[1],2))
    sum_exponent_v2 = (math.pow(v2[0],2))+(math.pow(v2[1],2))
@@ -12,7 +20,7 @@ def calculate_angle_between_vectors(v1,v2):
    module_v1 = math.sqrt(sum_exponent_v1)
    module_v2 = math.sqrt(sum_exponent_v2)
 
-   cos = (dot_product/(module_v1*module_v2))
+   cos = (calculate_dot_product(v1,v2)/(module_v1*module_v2))
    angle_cos=math.acos(cos)
    degree=angle_cos*(180/math.pi)
    return degree
